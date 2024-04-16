0bed84ce1ff4d64877971e0b279d8f89a2b6f44c
Naman2505 <naman.tiwari2505@gmail.com>
2024-04-16 12:53:38 +0530

    module2

--- module2/myApp.js
+++ module2/myApp.js
@@ -1,48 +1,26 @@
-let express = require('express');
+let express = require("express");
 require("dotenv").config();
 let app = express();
 
-app.use('/public', express.static(__dirname+"/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req,res)=>{
-    res.sendFile(__dirname+"/views/index.html");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+};
+
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/json",(req,res)=>{
-    if(process.env.MESSAGE_STYLE === "uppercase"){
-        res.json({"message":"Hello json".toUpperCase()});
-    }
-    else{
-        res.json({"message": "Hello json"});
-    }
-})
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "Hello json".toUpperCase() });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
